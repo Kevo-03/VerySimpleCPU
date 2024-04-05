821d1f0cf53135029a68482ccfacfad9b67c7333
Kevo-03 <115546321+Kevo-03@users.noreply.github.com>
2024-04-05 18:20:20 +0300

    Add files via upload

---
+++ VSCPU.v
@@ -0,0 +1,313 @@
+`timescale 1ns / 1ps
+
+module VSCPU (clk, rst, data_fromRAM, wrEn, addr_toRAM, data_toRAM);
+  input clk, rst;
+  output reg wrEn;
+  input [31:0] data_fromRAM;
+  output reg [31:0] data_toRAM;
+  output reg [13:0] addr_toRAM;
+  
+  reg [2:0] st, stN;
+  reg [13:0] PC, PCN;
+  reg [31:0] IW, IWN;
+  reg [31:0] R1, R1N;
+  
+  always @(posedge clk) begin
+    st <= stN;
+    PC <= PCN;
+    IW <= IWN;
+    R1 <= R1N;
+  end
+  
+  always @ * begin
+   if (rst) begin
+     stN = 3'd0;
+     PCN = 14'd0;
+   end
+   else begin
+     wrEn = 1'b0;
+     PCN = PC;
+     IWN = IW;
+     stN = 3'dx;
+     addr_toRAM= 14'hX;
+     data_toRAM= 32'hX;
+     R1N= 32'hX;
+
+     case (st) 
+     3'd0: begin // S0: Fetch State
+       addr_toRAM = PC;
+       stN = 3'd1;
+     end
+     3'd1: begin // S1: Decode State
+       IWN = data_fromRAM;
+       if(data_fromRAM[31:28] == 4'b0000)//ADD
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b0001)//ADDi
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b1000)//CP
+       begin
+         addr_toRAM = data_fromRAM[13:0];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b1001)//CPi only 1 state needed except fetch state
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = data_fromRAM[27:14];
+         data_toRAM = data_fromRAM[13:0];
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(data_fromRAM[31:28] == 4'b1010)//CPI
+       begin
+         addr_toRAM = data_fromRAM[13:0];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b1011)//CPIi
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b1100)//BZJ
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b1101)//BZJi
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b0011)//NANDi
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b0010)//NAND
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b0100)//SRL
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b0101)//SRLi
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b0110)//LT
+        begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+        end
+       if(data_fromRAM[31:28] == 4'b0111)//LTi
+        begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+        end
+       if(data_fromRAM[31:28] == 4'b1110)//MUL
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+       if(data_fromRAM[31:28] == 4'b1111)//MULi
+       begin
+         addr_toRAM = data_fromRAM[27:14];
+         stN = 3'd2;
+       end
+     end
+     3'd2: begin // S2: Decode/Execute State
+       if(IW[31:28] == 4'b0000)//ADD
+       begin
+         R1N = data_fromRAM;
+         addr_toRAM = IW[13:0];
+         stN = 3'd3;
+       end
+       if (IW[31:28]==4'b0001) begin // ADDi
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = data_fromRAM + IW[13:0];
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if (IW[31:28]==4'b1000) begin // CP
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = data_fromRAM;
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b1010)// CPI
+       begin
+         addr_toRAM = data_fromRAM;
+         stN=3'd3;
+       end
+       if(IW[31:28] == 4'b1011)// CPIi
+       begin
+         R1N = data_fromRAM;
+         addr_toRAM = IW[13:0];
+         stN = 3'd3;
+       end
+       if (IW[31:28]==4'b1100) begin // BZJ
+         R1N=data_fromRAM;
+         addr_toRAM = IW[13:0];
+         stN = 3'd3;
+       end
+       if (IW[31:28]==4'b1101) begin // BZJi
+         PCN = (data_fromRAM + IW[13:0]);
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b0011)//NANDi
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = ~(data_fromRAM & IW[13:0]);
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b0010)//NAND
+       begin
+         R1N = data_fromRAM;
+         addr_toRAM = IW[13:0];
+         stN = 3'd3;
+       end
+       if(IW[31:28] == 4'b0100)//SRL
+       begin
+         R1N = data_fromRAM;
+         addr_toRAM = IW[13:0];
+         stN = 3'd3;
+       end
+       if(IW[31:28] == 4'b0101)//SRLi
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = (IW[13:0] < 6'd32) ? (data_fromRAM >> IW[13:0]) : (data_fromRAM << (IW[13:0] - 6'd32));
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b0110)//LT
+        begin
+         R1N = data_fromRAM;
+         addr_toRAM = IW[13:0];
+         stN = 3'd3;
+        end
+       if(IW[31:28] == 4'b0111)//LTi
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = (data_fromRAM < IW[13:0]) ? 32'd1 : 32'd0;
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b1110)//MUL
+       begin
+         R1N = data_fromRAM;
+         addr_toRAM = IW[13:0];
+         stN = 3'd3;
+       end
+       if(IW[31:28] == 4'b1111)//MULi
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = (data_fromRAM * IW[13:0]);
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+     end
+     3'd3: begin // S3: Execute State
+       if(IW[31:28] == 4'b0000)//ADD
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = data_fromRAM + R1;
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if (IW[31:28]==4'b1100) begin // BZJ
+         if (data_fromRAM == 0)
+            PCN=R1;
+         else
+            PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b0010)//NAND
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = ~(data_fromRAM & R1);
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b0100)//SRL
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = (data_fromRAM < 6'd32) ? (R1 >> data_fromRAM) : (R1 << (data_fromRAM - 6'd32));
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b0110)//LT
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = (R1 < data_fromRAM) ? 32'd1 : 32'd0;
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b1110)//MUL
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = (R1 * data_fromRAM);
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b1010)// CPI
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = IW[27:14];
+         data_toRAM = data_fromRAM;
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+       if(IW[31:28] == 4'b1011)// CPIi
+       begin
+         wrEn = 1'b1;
+         addr_toRAM = R1;
+         data_toRAM = data_fromRAM;
+         PCN = PC + 14'd1;
+         stN = 3'd0;
+       end
+     end
+     endcase
+   end // else
+ end // always 	
+endmodule
+
+module blram(clk, rst, we, addr, din, dout);
+  parameter SIZE = 14, DEPTH = 2**SIZE;
+
+  input clk;
+  input rst;
+  input we;
+  input [SIZE-1:0] addr;
+  input [31:0] din;
+  output reg [31:0] dout;
+
+  reg [31:0] mem [DEPTH-1:0];
+
+  always @(posedge clk) begin
+    dout <= #1 mem[addr[SIZE-1:0]];
+    if (we)
+      mem[addr[SIZE-1:0]] <= #1 din;
+  end 
+endmodule
+
---
+++ tb_VSCPU.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+module tb_VSCPU;
+  parameter SIZE = 14, DEPTH = 2**SIZE;
+  reg clk, rst;
+  wire wrEn;
+  wire [SIZE-1:0] addr_toRAM;
+  wire [31:0] data_toRAM, data_fromRAM;
+
+  VSCPU uut (clk, rst, data_fromRAM, wrEn,  addr_toRAM, data_toRAM);
+
+  blram #(SIZE, DEPTH) inst_bram (
+clk, rst, wrEn, addr_toRAM, data_toRAM, data_fromRAM);
+
+  initial begin
+    clk = 1;
+    forever
+      #5 clk = ~clk;
+  end
+
+  initial begin
+    rst = 1;
+    repeat (10) @(posedge clk);
+    rst <= #1 0;
+    repeat (600) @(posedge clk);
+    $finish;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+    blram.mem[0] = 32'h190065;
+    blram.mem[1] = 32'h10190003;
+    blram.mem[2] = 32'h40190066;
+    blram.mem[3] = 32'h20190067;
+    blram.mem[4] = 32'h5019001d;
+    blram.mem[5] = 32'he0190065;
+    blram.mem[6] = 32'h60190066;
+    blram.mem[7] = 32'hf0190019;
+    blram.mem[8] = 32'h7019002d;
+    blram.mem[9] = 32'h80194064;
+    blram.mem[10] = 32'h90198007;
+    blram.mem[11] = 32'hf0198009;
+    blram.mem[12] = 32'h3019ffff;
+    blram.mem[13] = 32'ha019c03f;
+    blram.mem[14] = 32'hb019c03f;
+    blram.mem[15] = 32'hc01a0069;
+    blram.mem[16] = 32'h1019c00a;
+    blram.mem[17] = 32'h19c066;
+    blram.mem[18] = 32'hd01a0005;
+    blram.mem[23] = 32'hf01a0002;
+    blram.mem[40] = 32'h4b;
+    blram.mem[63] = 32'h28;
+    blram.mem[100] = 32'h5;
+    blram.mem[101] = 32'ha;
+    blram.mem[102] = 32'h3d;
+    blram.mem[103] = 32'hffffffff;
+    blram.mem[104] = 32'h12;
+    blram.mem[105] = 32'h0;
+
+
+  end
+endmodule
